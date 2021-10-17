db46d2bd7ba15a774ca42733049739471127d62e
philmish <thantalosdream@protonmail.com>
2021-10-17 22:33:00 +0200

    Added CLI command to start the server in different states (default, env_file)

---
+++ src/rake/main.py
@@ -0,0 +1,24 @@
+import click
+import uvicorn
+from rake.errors.cli import UnknownStartMode
+
+
+@click.command(name="up")
+@click.option("--mode", default="default", required=True)
+def up(mode):
+    if mode.lower() == "default":
+        from rake.config import RakeSettingsBase
+        settings = RakeSettingsBase()
+        
+    elif mode.lower() in ["env_file", "envfile", "env"]:
+        from rake.config import RakeSettingsEnvFile
+        settings = RakeSettingsEnvFile()
+
+    else:
+        raise UnknownStartMode(f"{mode} is not an accaptable start mode.")
+
+    from rake.server import app
+    uvicorn.run(app, host=settings.host, port=int(settings.port))        
+
+if __name__ == "__main__":
+    up()

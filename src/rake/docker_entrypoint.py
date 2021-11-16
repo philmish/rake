93febf214aeee640c1d73672dabc2932c69ad754
philmish <thantalosdream@protonmail.com>
2021-11-17 00:14:03 +0100

    Added entrypoint for docker container

---
+++ src/rake/docker_entrypoint.py
@@ -0,0 +1,5 @@
+import uvicorn
+from rake.config import RakeSettingsEnvVar
+settings = RakeSettingsEnvVar()
+from rake.server import app
+uvicorn.run(app, host=settings.host, port=int(settings.port))        
